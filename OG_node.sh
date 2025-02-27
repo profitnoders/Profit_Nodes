4ea57850ad1ada2203f47c35f291b4d1b666de2d
profitnoders <bangdolche@gmail.com>
2025-02-27 18:29:45 +0100

    Update OG_node.sh

--- OG_node.sh
+++ OG_node.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Отображение цветов 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-NC='\033[0m'  
+# Оформление текста: цвета и фоны
+CLR_INFO='\033[1;97;44m'  # Белый текст на синем фоне
+CLR_SUCCESS='\033[1;30;42m'  # Зеленый текст на черном фоне
+CLR_WARNING='\033[1;37;41m'  # Белый текст на красном фоне
+CLR_ERROR='\033[1;31;40m'  # Красный текст на черном фоне
+CLR_RESET='\033[0m'  # Сброс форматирования
 
 # Отображение лого 
-    echo -e "${GREEN}===============================${NC}"
-    echo -e "${CYAN} Добро пожаловать в скрипт установки ноды Zero Gravity ${NC}"
-    echo -e "${GREEN}===============================${NC}"
+    echo -e "${CLR_SUCCESS}===============================${CLR_RESET}"
+    echo -e "${CLR_SUCCESS} Добро пожаловать в скрипт установки ноды Zero Gravity ${CLR_RESET}"
+    echo -e "${CLR_SUCCESS}===============================${CLR_RESET}"
     curl -s https://raw.githubusercontent.com/profitnoders/Profit_Nodes/refs/heads/main/logo_new.sh | bash
 
 # Установка ноды OG
-echo -e "${CYAN}Устанавливаем ноду OG...${NC}"
+echo -e "${CLR_INFO}Устанавливаем ноду OG...${CLR_RESET}"
 if source <(curl -s https://raw.githubusercontent.com/zstake-xyz/test/refs/heads/main/0g_storage_installer.sh); then
-    echo -e "${GREEN}Нода успешно установлена!${NC}"
+    echo -e "${CLR_SUCCESS}Нода успешно установлена!${CLR_RESET}"
 else
-    echo -e "${RED}Ошибка при установки ноды .${NC}"
+    echo -e "${CLR_ERROR}Ошибка при установки ноды .${CLR_RESET}"
     exit 1
 fi
