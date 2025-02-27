eef34ed9126fe36d61ac77454ef7a8b6e9163bce
profitnoders <bangdolche@gmail.com>
2025-02-27 21:28:25 +0100

    Update waku_node.sh

--- waku_node.sh
+++ waku_node.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-# Цветовые обозначения для вывода сообщений
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m' # Сброс цвета
-
 # Функция отображения логотипа
 function show_logo() {
-    echo -e "${GREEN}==========================================================${NC}"
-    echo -e "${CYAN}        Добро пожаловать в скрипт управления нодой Waku        ${NC}"
-    echo -e "${GREEN}==========================================================${NC}"
+    echo -e "\033[1;30;42m==========================================================\033[0m"
+    echo -e "\033[1;97;44m        Добро пожаловать в скрипт управления нодой Waku        \033[0m"
+    echo -e "\033[1;30;42m==========================================================\033[0m"
     curl -s https://raw.githubusercontent.com/profitnoders/Profit_Nodes/refs/heads/main/logo_new.sh | bash
 }
 
 # Проверка и установка curl, если он отсутствует
 if ! command -v curl &> /dev/null; then
-    echo -e "${YELLOW}Устанавливаем curl...${NC}"
+    echo -e "\033[1;37;41mУстанавливаем curl...\033[0m"
     sudo apt update
     sudo apt install curl -y
 fi
 
 # Функция установки необходимых пакетов
 function install_dependencies() {
-    echo -e "${BLUE}Устанавливаем необходимые зависимости...${NC}"
     sudo apt update -y
     sudo apt upgrade -y
     sudo apt install -y curl iptables build-essential git wget jq make gcc nano tmux htop \
@@ -35,17 +25,13 @@ function install_dependencies() {
 
 # Функция установки Docker и Docker Compose
 function install_docker() {
-    echo -e "${BLUE}Проверяем Docker...${NC}"
     if ! command -v docker &> /dev/null; then
         curl -fsSL https://get.docker.com | sh
     fi
-
-    echo -e "${BLUE}Проверяем Docker Compose...${NC}"
     if ! command -v docker-compose &> /dev/null; then
-        echo -e "${YELLOW}Устанавливаем Docker Compose...${NC}"
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-    fi
+    sudo apt update && sudo apt install -y docker-compose
+    command -v docker-compose &> /dev/null && echo -e "\033[1;30;42mDocker Compose успешно установлен!\033[0m" || { echo -e "\033[1;31;40mОшибка установки.\033[0m"; exit 1; }
+
 }
 
 # Установка ноды Waku
@@ -58,22 +44,20 @@ function install_node() {
     cd nwaku-compose
     cp .env.example .env
 
-    echo -e "${YELLOW}Вставьте ваш RPC для Ethereum Sepolia:${NC}"
-    read RPC_URL
-    echo -e "${YELLOW}Ваш приватный ключ от EVM кошелька:${NC}"
-    read ETH_KEY
-    echo -e "${YELLOW}Установите пароль для RLN Membership:${NC}"
-    read RLN_PASSWORD
+    echo -e "\033[0;36;43mВставьте ваш RPC Sepolia ETH:\033[0m"
+    read RPC
+    echo -e "\033[0;36;43mВставьте ваш приватный ключ от EVM кошелька, на котором есть Sepolia ETH:\033[0m"
+    read PRIVATE_KEY
+    echo -e "\033[0;36;43mУстановите пароль:\033[0m"
+    read PASSWORD
 
-    sed -i "s|RLN_RELAY_ETH_CLIENT_ADDRESS=.*|RLN_RELAY_ETH_CLIENT_ADDRESS=$RPC_URL|" .env
-    sed -i "s|ETH_TESTNET_KEY=.*|ETH_TESTNET_KEY=$ETH_KEY|" .env
-    sed -i "s|RLN_RELAY_CRED_PASSWORD=.*|RLN_RELAY_CRED_PASSWORD=$RLN_PASSWORD|" .env
+    sed -i "s|RLN_RELAY_ETH_CLIENT_ADDRESS=.*|RLN_RELAY_ETH_CLIENT_ADDRESS=$RPC|" .env
+    sed -i "s|ETH_TESTNET_KEY=.*|ETH_TESTNET_KEY=$PRIVATE_KEY|" .env
+    sed -i "s|RLN_RELAY_CRED_PASSWORD=.*|RLN_RELAY_CRED_PASSWORD=$PASSWORD|" .env
 
     ./register_rln.sh
 
-    echo -e "${BLUE}Запускаем контейнеры Waku...${NC}"
     docker-compose up -d
-
 }
 
 # Обновление ноды Waku
@@ -85,36 +69,36 @@ function update_node() {
     ./register_rln.sh
     docker-compose up -d
 
-    echo -e "${GREEN}Обновление завершено!${NC}"
+    echo -e "\033[1;30;42mОбновление завершено!\033[0m"
 }
 
 # Просмотр логов ноды
 function view_logs() {
-    echo -e "${BLUE}Просмотр логов ноды Waku...${NC}"
-    cd $HOME/nwaku-compose
-    docker-compose logs -f
+    echo -e "\033[1;97;44mПросмотр логов ноды Waku...\033[0m"
+    cd $HOME/nwaku-compose && docker-compose logs -f
 }
 
 # Удаление ноды Waku
 function remove_node() {
-    echo -e "${BLUE}Удаляем ноду Waku...${NC}"
+    echo -e "\033[1;97;44mУдаляем ноду Waku...\033[0m"
     cd $HOME/nwaku-compose
     docker-compose down
     cd $HOME
     rm -rf nwaku-compose
-    echo -e "${GREEN}Нода успешно удалена!${NC}"
+    rm -rf waku_node.sh
+    echo -e "\033[1;30;42mНода успешно удалена!\033[0m"
 }
 
 # Главное меню
 function show_menu() {
     show_logo
-    echo -e "${CYAN}1) 🚀 Установить ноду${NC}"
-    echo -e "${CYAN}2) 🔄 Обновить ноду${NC}"
-    echo -e "${CYAN}3) 📜 Просмотр логов${NC}"
-    echo -e "${CYAN}4) 🗑️ Удалить ноду${NC}"
-    echo -e "${CYAN}5) ❌ Выйти${NC}"
+    echo -e "\033[0;36;43m1) 🚀 Установить ноду\033[0m"
+    echo -e "\033[0;36;43m2) 🔄 Обновить ноду\033[0m"
+    echo -e "\033[0;36;43m3) 📜 Просмотр логов\033[0m"
+    echo -e "\033[0;36;43m4) 🗑️ Удалить ноду\033[0m"
+    echo -e "\033[0;36;43m5) ❌ Выйти\033[0m"
 
-    echo -e "${YELLOW}Выберите действие:${NC}"
+    echo -e "\033[1;37;41mВыберите действие:\033[0m"
     read choice
 
     case $choice in
@@ -122,8 +106,8 @@ function show_menu() {
         2) update_node ;;
         3) view_logs ;;
         4) remove_node ;;
-        5) echo -e "${GREEN}Выход...${NC}" && exit 0 ;;
-        *) echo -e "${RED}Неверный выбор! Попробуйте снова.${NC}" && show_menu ;;
+        5) echo -e "\033[1;30;42mВыход...\033[0m" && exit 0 ;;
+        *) echo -e "\033[1;31;40mНеверный выбор! Попробуйте снова.\033[0m" && show_menu ;;
     esac
 }
 
