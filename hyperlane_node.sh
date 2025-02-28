e1eba37c3d8c197657bb579c0acbd0a3476168ef
profitnoders <bangdolche@gmail.com>
2025-02-28 09:38:14 +0100

    Update hyperlane_node.sh

--- hyperlane_node.sh
+++ hyperlane_node.sh
@@ -1,48 +1,42 @@
 #!/bin/bash
 
-# Цвета текста
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-NC='\033[0m' # Сброс цвета
+# Оформление текста: цвета и фоны
+CLR_INFO='\033[1;97;44m'  # Белый текст на синем фоне
+CLR_SUCCESS='\033[1;30;42m'  # Зеленый текст на черном фоне
+CLR_WARNING='\033[1;37;41m'  # Белый текст на красном фоне
+CLR_ERROR='\033[1;31;40m'  # Красный текст на черном фоне
+CLR_RESET='\033[0m'  # Сброс форматирования
+CLR_GREEN='\033[0;32m' #Зеленый текст
 
 # Функция для отображения логотипа
 function show_logo() {
-    echo -e "${GREEN}==========================================================${NC}"
-    echo -e "${CYAN}      Добро пожаловать в скрипт управления нодой Hyperlane      ${NC}"
-    echo -e "${GREEN}==========================================================${NC}"
+    echo -e "${CLR_INFO}      Добро пожаловать в скрипт управления нодой Hyperlane      ${CLR_RESET}"
     curl -s https://raw.githubusercontent.com/profitnoders/Profit_Nodes/refs/heads/main/logo_new.sh | bash
 }
 
 # Проверка и установка необходимых пакетов
 function install_dependencies() {
-    echo -e "${YELLOW}Обновляем систему и устанавливаем необходимые зависимости...${NC}"
     sudo apt update && sudo apt upgrade -y
     if ! command -v docker &> /dev/null; then
-        echo -e "${YELLOW}Установка Docker...${NC}"
         sudo apt install docker.io -y
     else
-        echo -e "${GREEN}Docker уже был установлен.${NC}"
+        echo -e "${CLR_SUCCESS}Docker уже установлен.${CLR_RESET}"
     fi
 }
 
 # Установка ноды
 function install_node() {
-    echo -e "${GREEN}Начинаем установку ноды Hyperlane...${NC}"
     install_dependencies
 
-    # Загрузка Docker-образа
-    echo -e "${YELLOW}Загружаем Docker-образ ноды...${NC}"
+    echo -e "${CLR_INFO}Загружаем Docker-образ ноды...${CLR_RESET}"
     docker pull --platform linux/amd64 gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0
 
     # Запрос данных у пользователя
-    echo -e "${YELLOW}Введите имя валидатора:${NC}"
+    echo -e "Введите имя валидатора:"
     read -r VALIDATOR_NAME
-    echo -e "${YELLOW}Введите приватный ключ вашего EVM кошелька (начиная с 0x):${NC}"
+    echo -e "Введите private key EVM кошелька:"
     read -r PRIVATE_KEY
-    echo -e "${YELLOW}Введите вашу RPC для сети Base Mainnet:${NC}"
+    echo -e "Введите вашу RPC для сети Base Mainnet:"
     read -r BASE_MAINNET_RPC
 
     # Создание рабочей директории
@@ -65,51 +59,44 @@ function install_node() {
         --chains.base.signer.key "$PRIVATE_KEY" \
         --chains.base.customRpcUrls "$BASE_MAINNET_RPC,wss://base-rpc.publicnode.com,http://rpc-base-node-url.com"
 
-    echo -e "${GREEN}Нода Hyperlane успешно установлена и запущена!${NC}"
+    echo -e "${CLR_SUCCESS}Нода Hyperlane успешно установлена и запущена!${CLR_RESET}"
 }
 
 # Обновление ноды
 function update_node() {
-    echo -e "${BLUE}Обновляем ноду Hyperlane...${NC}"
     docker pull --platform linux/amd64 gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0
-    echo -e "${GREEN}Нода успешно обновлена до последней версии!${NC}"
+    echo -e "${CLR_SUCCESS}Нода успешно обновлена до последней версии!${CLR_RESET}"
 }
 
 # Просмотр логов
 function view_logs() {
-    echo -e "${BLUE}Открываем логи ноды Hyperlane...${NC}"
+    echo -e "${CLR_INFO}Логи ноды Hyperlane...${CLR_RESET}"
     docker logs --tail 100 -f hyperlane
 }
 
 # Удаление ноды
 function remove_node() {
-    echo -e "${RED}Удаление ноды Hyperlane...${NC}"
-
-    # Остановка и удаление Docker-контейнера
     docker stop hyperlane
     docker rm hyperlane
-
-    # Удаление данных ноды
     if [ -d "$HOME/hyperlane_db_base" ]; then
         rm -rf $HOME/hyperlane_db_base
-        echo -e "${GREEN}Директория ноды удалена.${NC}"
+        echo -e "${CLR_SUCCESS}Нода Hyperlane удалена.${CLR_RESET}"
     else
-        echo -e "${RED}Директория ноды не найдена.${NC}"
+        echo -e "${CLR_ERROR}Директория ноды не найдена.${CLR_RESET}"
     fi
 
-    echo -e "${GREEN}Нода Hyperlane успешно удалена!${NC}"
 }
 
 # Меню управления
 function show_menu() {
     show_logo
-    echo -e "${CYAN}1) 🚀 Установить ноду${NC}"
-    echo -e "${CYAN}2) 🔄 Обновить ноду${NC}"
-    echo -e "${CYAN}3) 📜 Просмотр логов${NC}"
-    echo -e "${CYAN}4) 🗑️ Удалить ноду${NC}"
-    echo -e "${CYAN}5) ❌ Выйти${NC}"
+    echo -e "${CLR_GREEN}1) 🚀 Установить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}2) 🔄 Обновить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}3) 📜 Просмотр логов${CLR_RESET}"
+    echo -e "${CLR_GREEN}4) 🗑️ Удалить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}5) ❌ Выйти${CLR_RESET}"
 
-    echo -e "${YELLOW}Введите номер действия:${NC}"
+    echo -e "${CLR_INFO}Введите номер действия:${CLR_RESET}"
     read -r choice
 
     case $choice in
@@ -117,8 +104,8 @@ function show_menu() {
         2) update_node ;;
         3) view_logs ;;
         4) remove_node ;;
-        5) echo -e "${GREEN}Выход...${NC}" ;;
-        *) echo -e "${RED}Неверный выбор! Попробуйте снова.${NC}" ;;
+        5) echo -e "${CLR_ERROR}Выход...${CLR_RESET}" ;;
+        *) echo -e "${CLR_WARNING}Неверный выбор! Попробуйте снова.${CLR_RESET}" ;;
     esac
 }
 
