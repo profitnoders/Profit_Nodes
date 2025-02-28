86edbf12d829680f139d32ad6c75e777311af82f
profitnoders <bangdolche@gmail.com>
2025-02-28 10:19:06 +0100

    Update t3rn_node.sh

--- t3rn_node.sh
+++ t3rn_node.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
-# Определяем цвета для текста
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m' # Сброс цвета
+# Оформление текста: цвета и фоны
+CLR_INFO='\033[1;97;44m'  # Белый текст на синем фоне
+CLR_SUCCESS='\033[1;30;42m'  # Зеленый текст на черном фоне
+CLR_WARNING='\033[1;37;41m'  # Белый текст на красном фоне
+CLR_ERROR='\033[1;31;40m'  # Красный текст на черном фоне
+CLR_RESET='\033[0m'  # Сброс форматирования
+CLR_GREEN='\033[0;32m' #Зеленый текст
 
 # Функция отображения логотипа
 function show_logo() {
-    echo -e "${GREEN}==========================================================${NC}"
-    echo -e "${CYAN}       Добро пожаловать в скрипт управления нодой t3rn       ${NC}"
-    echo -e "${GREEN}==========================================================${NC}"
+    echo -e "${CLR_INFO}       Добро пожаловать в скрипт управления нодой t3rn       ${CLR_RESET}"
     curl -s https://raw.githubusercontent.com/profitnoders/Profit_Nodes/refs/heads/main/logo_new.sh | bash
-}
-
-# Проверка версии Ubuntu
-function check_ubuntu_version() {
-    UBUNTU_VERSION=$(lsb_release -rs)
-    REQUIRED_VERSION=22.04
-
-    if (( $(echo "$UBUNTU_VERSION < $REQUIRED_VERSION" | bc -l) )); then
-        echo -e "${RED}Для установки требуется Ubuntu версии 22.04 или выше.${NC}"
-        exit 1
-    fi
 }
 
 # Установка необходимых зависимостей
 function install_dependencies() {
-    echo -e "${BLUE}Устанавливаем необходимые пакеты...${NC}"
     sudo apt update -y
     sudo apt upgrade -y
     sudo apt install -y curl figlet jq build-essential gcc unzip wget lz4 bc
@@ -38,17 +23,16 @@ function install_dependencies() {
 
 # Установка ноды t3rn
 function install_node() {
-    check_ubuntu_version
     install_dependencies
 
-    echo -e "${BLUE}Скачиваем и устанавливаем последнюю версию ноды t3rn...${NC}"
+    echo -e "${CLR_INFO}Скачиваем и устанавливаем последнюю версию ноды t3rn...${CLR_RESET}"
     LATEST_VERSION=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep 'tag_name' | cut -d\" -f4)
     EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/${LATEST_VERSION}/executor-linux-${LATEST_VERSION}.tar.gz"
     curl -L -o executor-linux-${LATEST_VERSION}.tar.gz $EXECUTOR_URL
     tar -xzvf executor-linux-${LATEST_VERSION}.tar.gz
     rm -rf executor-linux-${LATEST_VERSION}.tar.gz
 
-    echo -e "${YELLOW}Введите ваш приватный ключ от кошелька для ноды:${NC}"
+    echo -e "${CLR_WARNING}Введите ваш private key от EVM кошелька, на котором есть ETH Sepolia:${CLR_RESET}"
     read -r PRIVATE_KEY
 
     CONFIG_FILE="$HOME/executor/executor/bin/.t3rn"
@@ -84,17 +68,13 @@ EOT"
     sudo systemctl enable t3rn
     sudo systemctl start t3rn
 
-    echo -e "${GREEN}Установка завершена!${NC}"
-    echo -e "${YELLOW}Для проверки логов выполните:${NC} sudo journalctl -u t3rn -f"
+    echo -e "${CLR_SUCCESS}Установка завершена!${CLR_RESET}"
 }
 
 # Обновление ноды t3rn
-function update_node() {
-    echo -e "${BLUE}Обновляем ноду t3rn до последней версии...${NC}"
-    
+function update_node() {    
     sudo systemctl stop t3rn
-    cd
-    rm -rf executor/
+    cd && rm -rf executor/
     # Скачиваем новый бинарник
     LATEST_VERSION=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep 'tag_name' | cut -d\" -f4)
     EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/${LATEST_VERSION}/executor-linux-${LATEST_VERSION}.tar.gz"
@@ -115,7 +95,7 @@ function update_node() {
     echo "ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn'" >> $CONFIG_FILE
     echo "RPC_ENDPOINTS_BSSP='https://base-sepolia-rpc.publicnode.com'" >> $CONFIG_FILE
 
-    echo -e "${YELLOW}Введите ваш приватный ключ от кошелька для ноды:${NC}"
+    echo -e "${YELLOW}Введите ваш приватный ключ от кошелька для ноды:${CLR_RESET}"
     read PRIVATE_KEY
     sed -i "s|PRIVATE_KEY_LOCAL=|PRIVATE_KEY_LOCAL=$PRIVATE_KEY|" $CONFIG_FILE
     sudo systemctl daemon-reload
@@ -123,35 +103,35 @@ function update_node() {
     sudo systemctl start t3rn
     sleep 1
 
-    echo -e "${GREEN}Обновление завершено!${NC}"
+    echo -e "${CLR_SUCCESS}Обновление завершено!${CLR_RESET}"
 }
 
 # Удаление ноды t3rn
 function remove_node() {
-    echo -e "${RED}Удаляем ноду t3rn...${NC}"
+    echo -e "${CLR_ERROR}Удаляем ноду t3rn...${CLR_RESET}"
     sudo systemctl stop t3rn
     sudo systemctl disable t3rn
     sudo rm /etc/systemd/system/t3rn.service
     rm -rf $HOME/executor/
-    echo -e "${GREEN}Нода успешно удалена!${NC}"
+    echo -e "${CLR_SUCCESS}Нода успешно удалена!${CLR_RESET}"
 }
 
 # Просмотр логов
 function view_logs() {
-    echo -e "${BLUE}Открываем логи ноды...${NC}"
+    echo -e "${CLR_INFO}Логи ноды t3rn...${CLR_RESET}"
     sudo journalctl -u t3rn -f
 }
 
 # Главное меню
 function show_menu() {
     show_logo
-    echo -e "${CYAN}1) 🚀 Установить ноду${NC}"
-    echo -e "${CYAN}2) 🔄 Обновить ноду${NC}"
-    echo -e "${CYAN}3) 📜 Просмотр логов${NC}"
-    echo -e "${CYAN}4) 🗑️ Удалить ноду${NC}"
-    echo -e "${CYAN}5) ❌ Выйти${NC}"
+    echo -e "${CLR_GREEN}1) 🚀 Установить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}2) 🔄 Обновить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}3) 📜 Просмотр логов${CLR_RESET}"
+    echo -e "${CLR_GREEN}4) 🗑️ Удалить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}5) ❌ Выйти${CLR_RESET}"
 
-    echo -e "${YELLOW}Выберите действие:${NC}"
+    echo -e "${CLR_INFO}Выберите действие:${CLR_RESET}"
     read -r choice
 
     case $choice in
@@ -159,8 +139,8 @@ function show_menu() {
         2) update_node ;;
         3) view_logs ;;
         4) remove_node ;;
-        5) echo -e "${GREEN}Выход...${NC}" && exit 0 ;;
-        *) echo -e "${RED}Неверный выбор! Попробуйте снова.${NC}" && show_menu ;;
+        5) echo -e "${CLR_SUCCESS}Выход...${CLR_RESET}" && exit 0 ;;
+        *) echo -e "${CLR_ERROR}Неверный выбор! Попробуйте снова.${CLR_RESET}" && show_menu ;;
     esac
 }
 
