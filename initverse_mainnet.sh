898d172a3df59bd0bc7697107b0062c98b58d2da
profitnoders <bangdolche@gmail.com>
2025-02-28 10:30:05 +0100

    Update initverse_mainnet.sh

--- initverse_mainnet.sh
+++ initverse_mainnet.sh
@@ -1,52 +1,51 @@
 #!/bin/bash
 
-# Цветовые коды для отображения информации
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-NC='\033[0m' # Сброс цвета
+# Оформление текста: цвета и фоны
+CLR_INFO='\033[1;97;44m'  # Белый текст на синем фоне
+CLR_SUCCESS='\033[1;30;42m'  # Зеленый текст на черном фоне
+CLR_WARNING='\033[1;37;41m'  # Белый текст на красном фоне
+CLR_ERROR='\033[1;31;40m'  # Красный текст на черном фоне
+CLR_RESET='\033[0m'  # Сброс форматирования
+CLR_GREEN='\033[0;32m' #Зеленый текст
 
 # Функция отображения логотипа
 function show_logo() {
-    echo -e "${GREEN}==========================================================${NC}"
-    echo -e "${CYAN}       Добро пожаловать в скрипт управления InitVerse Mainnet       ${NC}"
-    echo -e "${GREEN}==========================================================${NC}"
+    echo -e "${CLR_INFO}       Добро пожаловать в скрипт управления InitVerse Mainnet       ${CLR_RESET}"
     curl -s https://raw.githubusercontent.com/profitnoders/Profit_Nodes/refs/heads/main/logo_new.sh | bash
 }
 
-# Проверка и установка curl, если его нет
-if ! command -v curl &> /dev/null; then
-    sudo apt update
-    sudo apt install curl -y
-fi
+
 
 # Функция установки зависимостей
 function install_dependencies() {
     sudo apt update -y
     sudo apt upgrade -y
     sudo apt install -y wget
+
+    # Проверка и установка curl, если его нет
+    if ! command -v curl &> /dev/null; then
+        sudo apt update
+        sudo apt install curl -y
+    fi
 }
 
 # Установка ноды InitVerse Mainnet
 function install_node() {
     install_dependencies
 
-    echo -e "${BLUE}Создаем директорию для ноды${NC}"
     mkdir -p $HOME/initverse
     cd $HOME/initverse
     wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/iniminer-linux-x64
     chmod +x iniminer-linux-x64
 
-    echo -e "${YELLOW}Вставьте адрес вашего EVM кошелька:${NC}"
+    echo -e "${CLR_WARNING}Введите имя для майнера:${CLR_RESET}"
+    read MAINER_NAME
+    echo -e "${CLR_WARNING}Вставьте EVM-адрес кошелька:${CLR_RESET}"
     read WALLET
-    echo -e "${YELLOW}Введите имя для майнера:${NC}"
-    read NODE_NAME
 
     # Создаем файл конфигурации .env
     echo "WALLET=$WALLET" > "$HOME/initverse/.env"
-    echo "NODE_NAME=$NODE_NAME" >> "$HOME/initverse/.env"
+    echo "MAINER_NAME=$MAINER_NAME" >> "$HOME/initverse/.env"
 
     # Создаем системный сервис
     sudo bash -c "cat <<EOT > /etc/systemd/system/initverse.service
@@ -58,7 +57,7 @@ After=network.target
 User=$(whoami)
 WorkingDirectory=$HOME/initverse
 EnvironmentFile=$HOME/initverse/.env
-ExecStart=/bin/bash -c 'source $HOME/initverse/.env && $HOME/initverse/iniminer-linux-x64 --pool stratum+tcp://${WALLET}.${NODE_NAME}@pool-c.yatespool.com:31189 --cpu-devices 1 --cpu-devices 2'
+ExecStart=/bin/bash -c 'source $HOME/initverse/.env && $HOME/initverse/iniminer-linux-x64 --pool stratum+tcp://${WALLET}.${MAINER_NAME}@pool-c.yatespool.com:31189 --cpu-devices 1 --cpu-devices 2'
 Restart=on-failure
 
 [Install]
@@ -69,21 +68,18 @@ EOT"
     sudo systemctl daemon-reload
     sudo systemctl enable initverse
     sudo systemctl start initverse
-    echo -e "${GREEN}Нода InitVerse в сети Mainnet установлена !${NC}"
+    echo -e "${CLR_SUCCESS}Нода InitVerse в сети Mainnet установлена !${CLR_RESET}"
 }
 
 
 # Просмотр логов
 function view_logs() {
-    echo -e "${BLUE}Логи InitVerse Mainnet...${NC}"
+    echo -e "${CLR_INFO}Логи InitVerse Mainnet...${CLR_RESET}"
     sudo journalctl -fu initverse.service
 }
 
 # Удаление ноды InitVerse Mainnet
 function remove_node() {
-    echo -e "${BLUE}Удаление ноды InitVerse Mainnet...${NC}"
-
-    # Остановка и удаление сервиса
     sudo systemctl stop initverse
     sudo systemctl disable initverse
     sudo rm /etc/systemd/system/initverse.service
@@ -92,27 +88,27 @@ function remove_node() {
     # Удаление файлов ноды
     if [ -d "$HOME/initverse" ]; then
         rm -rf $HOME/initverse
-        echo -e "${GREEN}Все файлы ноды InitVerse Mainnet удалены.${NC}"
+        echo -e "${CLR_WARNING}Все файлы ноды InitVerse Mainnet удалены.${CLR_RESET}"
     fi
 }
 
 # Главное меню
 function show_menu() {
     show_logo
-    echo -e "${CYAN}1) 🚀 Установить ноду${NC}"
-    echo -e "${CYAN}2) 📜 Просмотр логов${NC}"
-    echo -e "${CYAN}3) 🗑️ Удалить ноду${NC}"
-    echo -e "${CYAN}4) ❌ Выйти${NC}"
+    echo -e "${CLR_GREEN}1) 🚀 Установить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}2) 📜 Просмотр логов${CLR_RESET}"
+    echo -e "${CLR_GREEN}3) 🗑️ Удалить ноду${CLR_RESET}"
+    echo -e "${CLR_GREEN}4) ❌ Выйти${CLR_RESET}"
 
-    echo -e "${YELLOW}Выберите номер действия:${NC}"
+    echo -e "${CLR_INFO}Выберите номер действия:${CLR_RESET}"
     read choice
 
     case $choice in
         1) install_node ;;
         2) view_logs ;;
         3) remove_node ;;
-        4) echo -e "${GREEN}Выход...${NC}" && exit 0 ;;
-        *) echo -e "${RED}Неверный выбор. Попробуйте снова.${NC}" && show_menu ;;
+        4) echo -e "${CLR_ERROR}Выход...${CLR_RESET}" && exit 0 ;;
+        *) echo -e "${CLR_WARNING}Неверный выбор. Попробуйте снова.${CLR_RESET}" && show_menu ;;
     esac
 }
 
