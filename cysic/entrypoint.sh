0b105765ac4a78e96452c62f0710a545dd400538
profitnoders <bangdolche@gmail.com>
2025-06-18 20:59:45 +0400

    Update entrypoint.sh

--- cysic/entrypoint.sh
+++ cysic/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
 set -e
 
-echo "▶ Запуск Cysic Verifier..."
-if [ ! -f "/root/start.sh" ]; then
-    echo "❌ Не найден start.sh в /root. Проверьте корректность инициализации!"
+if [[ -z "$REWARD_ADDRESS" ]]; then
+    echo "❌ REWARD_ADDRESS не задан!"
     exit 1
 fi
 
-chmod +x /root/start.sh
-exec bash /root/start.sh
+# Скачиваем и запускаем официальный скрипт
+curl -L https://github.com/cysic-labs/cysic-phase3/releases/download/v1.0.0/setup_linux.sh -o /opt/cysic/setup_linux.sh
+chmod +x /opt/cysic/setup_linux.sh
+/opt/cysic/setup_linux.sh "$REWARD_ADDRESS"
+
+cd /root/cysic-verifier
+bash start.sh
+
